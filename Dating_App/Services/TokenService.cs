42ab1efadc18e6bef1f6b8b4b8990fbc69b8ac84
Ahmed.Hamada <Ahmed.Hamada@AHMED-HAMADA>
2022-02-17 17:08:50 +0200

    hk

---
+++ Dating_App/Services/TokenService.cs
@@ -0,0 +1,17 @@
+﻿using Dating_App.Entities;
+using Dating_App.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dating_App.Services
+{
+    public class TokenService : ITokenService
+    {
+        public string CreateTocken(AppUser user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
