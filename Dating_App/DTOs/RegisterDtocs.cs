0588978ed18839a17a8170818bcc6691d892a437
Ahmed.Hamada <Ahmed.Hamada@AHMED-HAMADA>
2022-03-15 16:27:27 +0200

    RELATION

--- Dating_App/DTOs/RegisterDtocs.cs
+++ Dating_App/DTOs/RegisterDtocs.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,10 @@ namespace Dating_App.DTOs
 {
     public class RegisterDtocs
     {
+        [Required]
         public string Username { get; set; }
+        [Required]
+        [StringLength(8,MinimumLength =4)]
         public string Password { get; set; }
     }
 }
